749d75fa77e97024b797b5fa36dc26778b9a1056
const <semita.recta.est@gmail.com>
2020-02-04 11:05:55 -0800

    nash experiments

--- Rank.py
+++ Rank.py
@@ -1,16 +1,14 @@
 
 import argparse
-import math
-import numpy
-from numpy import linalg as LA
-import random
-import sys
-from scipy import spatial
 import tensorflow as tf
 import types
-numpy.set_printoptions(precision=3, suppress=True, threshold=sys.maxsize, linewidth=sys.maxsize)
+import random
+import numpy
+from numpy import linalg as LA
+numpy.set_printoptions(precision=4, suppress=True)
 
-def alloc(mode, hessians, init_weights, alphas, hparams):
+
+def alloc(mode, hessians, alphas, hparams):
     graph = tf.Graph()
     session = tf.Session(graph=graph)
     n = hparams.n_nodes
@@ -27,17 +25,17 @@ def alloc(mode, hessians, init_weights, alphas, hparams):
         # Weights: W: Weight matix bids.
         # W = [n x n]
         w_list = []
-        for i in range(n):
-            w_i = tf.Variable(init_weights[i], dtype=tf.float32)
+        for _ in range(n):
+            w_i = tf.Variable(tf.random.uniform((1, n), minval=0))
             w_list.append(w_i)
         W_concat = tf.concat(w_list, axis=0) # Build full matrix.
-        W_clip = tf.clip_by_value(W_concat, 0, 1)
-        W = tf.linalg.normalize(W_clip, ord=1, axis=1)[0] # Sum to 1.
+        W_sig = tf.sigmoid(W_concat) # Force onto [0,1]
+        W = tf.linalg.normalize(W_sig, ord=1, axis=1)[0] # Sum to 1.
 
         # Mask: M: The mask to apply over inputs F.
         # Q = [n x n]
         shift = tf.reduce_mean(W, axis=0)
-        M = tf.clip_by_value(tf.nn.relu(W - shift)*10, 0, 1)
+        M = tf.nn.relu6(W - shift)
 
         # Loss: L: The change to each loss effected by the Mask.
         # L = [n]
@@ -50,15 +48,15 @@ def alloc(mode, hessians, init_weights, alphas, hparams):
             l_list.append(l_i)
         L = tf.concat(l_list, axis=0)
 
-
         # Utility: U: The utility gained or lost via the loss.
         U = L
 
-        Ws = tf.reduce_sum(W, axis=0, keepdims=True)
-        Wr = tf.div(tf.subtract(Ws, tf.reduce_min(Ws)), tf.subtract(tf.reduce_max(Ws), tf.reduce_min(Ws)))
-        AR = Wr * (1 - alphas)
-        R = tf.squeeze(tf.linalg.normalize(Wr, ord=1, axis=1)[0]) + 0.00001
-        AR = tf.squeeze(tf.linalg.normalize(AR, ord=1, axis=1)[0]) + 0.00001
+        # Ranking: R : The ranking score.
+        #W_dg = tf.matrix_set_diag(tf.zeros((n,n)), tf.linalg.tensor_diag_part(W), k = 0)
+        #W = W - W_dg
+        #R = tf.squeeze(tf.linalg.normalize(tf.reduce_sum(W, axis=0, keepdims=True), ord=1, axis=1)[0])
+        R = tf.nn.softmax(tf.reduce_sum(W, axis=0))
+
 
         # Divergence score: D: Divergence of each ranking from mean.
         # D = [n]
@@ -66,10 +64,11 @@ def alloc(mode, hessians, init_weights, alphas, hparams):
         cross_entropy = -tf.reduce_sum(tf.multiply(W_avg, tf.log(W)), axis=1)
         D = tf.nn.softmax(tf.reshape(cross_entropy, [n]))
 
-        # Payoff
-        P =  U * alphas + R * (1 - alphas)
+        # Payoff: P: U + R - D
+        #P =  U #* alphas + R * (1 - alphas) * 0.001 # - D
+        #P = U + R * 5
+        P = U # * alphas  - D * (1 - alphas)
 
-        # Sorted Ranking
         topk = tf.math.top_k(R, k=n, sorted=True)[1]
 
         ### Bellow Optimization.
@@ -79,7 +78,7 @@ def alloc(mode, hessians, init_weights, alphas, hparams):
 
         # Mode == Competitive: All nodes optimize only their local payoff.
         train_steps = []
-        if mode == 'competitive' :
+        if mode == 'competitive':
             for i in range(hparams.n_nodes):
                 p_i = tf.slice(P, [i], [1])
                 w_i = w_list[i]
@@ -91,21 +90,13 @@ def alloc(mode, hessians, init_weights, alphas, hparams):
             grads_and_vars = optimizer.compute_gradients(loss=-tf.reduce_mean(P), var_list=w_list)
             train_steps.append(optimizer.apply_gradients(grads_and_vars))
 
-
-        elif mode == 'idealized':
-            grads_and_vars = optimizer.compute_gradients(loss=-tf.reduce_mean(U), var_list=w_list)
-            train_steps.append(optimizer.apply_gradients(grads_and_vars))
-
-
         # Init the graph.
         session.run(tf.global_variables_initializer())
 
+        # Converge...
+        for step in range(hparams.n_steps):
 
-        if mode == 'competitive':
-            hparams.max_steps = hparams.max_steps * 10
 
-
-        for step_i in range(hparams.max_steps):
 
             # Randomly choose participant to optimize
             if mode == 'competitive':
@@ -113,10 +104,6 @@ def alloc(mode, hessians, init_weights, alphas, hparams):
 
             # Optimize all participants.
             elif mode == 'coordinated':
-                step = train_steps[0]
-
-            # Optimize all participants.
-            elif mode == 'idealized':
                 step = train_steps[0]
 
             # Run graph.
@@ -127,15 +114,13 @@ def alloc(mode, hessians, init_weights, alphas, hparams):
                                         'U': U,
                                         'D': D,
                                         'R': R,
-                                        'AR': AR,
                                         'W': W,
                                         'M': M,
                                         'topk': topk,
                                       })
 
-            if step_i % 500 == 0:
-                print (output['W'])
-                print (output['topk'])
+            print (output['M'])
+            print (output['topk'])
 
         # Return metrics.
         return output
@@ -152,6 +137,20 @@ def kl(p, q):
 
     return numpy.sum(numpy.where(p != 0, p * numpy.log(p / q), 0))
 
+def Rank(hparams, hessians):
+    rank_j = []
+    identity = numpy.eye(hparams.n_nodes)
+    for j in range(hparams.n):
+        mask = identity[i:-1]
+        j_sum = 0
+        for i in range(hparams.n_nodes):
+            i_sum = 0
+            for hess in Hessians:
+                 cost_sum += numpy.sum(numpy.matmul(hess, mask))
+            j_sum += i_sum
+        rank_j.append(j_sum)
+    return rank_j
+
 def trial(hparams):
     # Hessians: H: The hessian of the loss w.r.t a change in weights.
     # via second order taylor series approximation. First term is 0 at convergence.
@@ -164,22 +163,9 @@ def trial(hparams):
         h_i = numpy.random.randn(hparams.n_nodes, hparams.n_nodes)
         h_i = (h_i - numpy.min(h_i))/numpy.ptp(h_i)
         h_i = h_i/h_i.sum(axis=1, keepdims=1)
-    #    print (h_i)
-    #    print ('')
+        print (h_i)
+        print ('')
         hessians.append(h_i)
-    print ('')
-
-    # Initial Weights: W: The bids.
-    # W = [n x n]
-    print ('Weights: W')
-    weights = []
-    for i in range(hparams.n_nodes):
-        w_i = numpy.random.randn(1, hparams.n_nodes)
-        w_i = (w_i - numpy.min(w_i))/numpy.ptp(w_i)
-        w_i = w_i/w_i.sum(axis=1, keepdims=1)
-        #print (w_i)
-        #print ('')
-        weights.append(w_i)
     print ('')
 
     # Alphas: A: The trade off between optimizing Utility vs optimizing for
@@ -188,21 +174,14 @@ def trial(hparams):
     print ('P = αU + (1- α)R')
     alphas = numpy.random.randn(hparams.n_nodes)
     alphas = (alphas - numpy.min(alphas))/numpy.ptp(alphas)
-    #print (alphas)
+    print (alphas)
     print ('')
 
     # Run coordinated weight convergence.
-    ideal_output = alloc('idealized', hessians, weights, alphas, hparams)
-
-    # Run coordinated weight convergence.
-    coord_output = alloc('coordinated', hessians, weights, alphas, hparams)
+    coord_output = alloc('coordinated', hessians, alphas, hparams)
 
     # Run competitive weight convergence.
-    comp_output = alloc('competitive', hessians, weights, alphas, hparams)
-
-    print ('Idealized Weights: W')
-    print (ideal_output['W'])
-    print ('')
+    comp_output = alloc('competitive', hessians, alphas, hparams)
 
     print ('Coordinated Weights: W')
     print (coord_output['W'])
@@ -210,11 +189,6 @@ def trial(hparams):
 
     print ('Competitive Weights: W')
     print (comp_output['W'])
-    print ('')
-
-    print ('Idealized Mask: M')
-    print ('M = σ ( W - avg(W) )')
-    print (ideal_output['M'])
     print ('')
 
     print ('Coordinated Mask: M')
@@ -234,24 +208,14 @@ def trial(hparams):
     print (alphas)
     print ('')
 
-    # print ('Idealized Divergence: D')
-    # print ('D = KL ( Q, avg(Q) )')
-    # print (ideal_output['D'])
-    # print ('')
-    #
-    # print ('Coordinated Divergence: D')
-    # print ('D = KL ( Q, avg(Q) )')
-    # print (coord_output['D'])
-    # print ('')
-    #
-    # print ('Competitive Divergence: D')
-    # print ('D = KL ( Q, avg(Q) )')
-    # print (comp_output['D'])
-    # print ('')
+    print ('Coordinated Divergence: D')
+    print ('D = KL ( Q, avg(Q) )')
+    print (coord_output['D'])
+    print ('')
 
-    print ('Idealized Ranking: R')
-    print ('softmax(sum(W, axis=0))')
-    print (ideal_output['R'])
+    print ('Competitive Divergence: D')
+    print ('D = KL ( Q, avg(Q) )')
+    print (comp_output['D'])
     print ('')
 
     print ('Coordinated Ranking: R')
@@ -264,27 +228,6 @@ def trial(hparams):
     print (comp_output['R'])
     print ('')
 
-    print ('Idealized Ranking: AR')
-    print ('softmax(sum(W, axis=0))')
-    print (ideal_output['AR'])
-    print ('')
-
-    print ('Coordinated Ranking: AR')
-    print ('softmax(sum(W, axis=0))')
-    print (coord_output['AR'])
-    print ('')
-
-    print ('Competitive Ranking: AR')
-    print ('softmax(sum(W, axis=0))')
-    print (comp_output['AR'])
-    print ('')
-
-
-    print ('Idealized Utility: U')
-    print ('U = M^t * H * M')
-    print (ideal_output['U'])
-    print ('')
-
     print ('Coordinated Utility: U')
     print ('U = M^t * H * M')
     print (coord_output['U'])
@@ -293,12 +236,6 @@ def trial(hparams):
     print ('Competitive Utility: U')
     print ('U = M^t * H * M')
     print (comp_output['U'])
-    print ('')
-
-    print ('Idealized Payoff: P')
-    print ('P = U')
-    print (ideal_output['P'])
-    print ('Avg:' + str(sum(ideal_output['P'])/hparams.n_nodes))
     print ('')
 
     print ('Coordinated Payoff: P')
@@ -313,34 +250,14 @@ def trial(hparams):
     print ('Avg:' + str(sum(comp_output['P'])/hparams.n_nodes))
     print ('')
 
-    print ('Idealized sorted: ' + str(ideal_output['topk']))
-    print ('')
-
     print ('Coordinated sorted: ' + str(coord_output['topk']))
     print ('')
 
     print ('Competitve sorted: ' + str(comp_output['topk']))
     print ('')
 
-    coord_divergence = kl(ideal_output['R'], coord_output['R'])
-    print ('Coordinated Ranking KL Divergence: ' + str(coord_divergence))
-    print ('')
-
-    comp_divergence = kl(ideal_output['R'], comp_output['R'])
-    print ('Competitve Ranking KL Divergence: ' + str(comp_divergence))
-    print ('')
-
-
-    coord_divergence = kl(ideal_output['R'], coord_output['AR'])
-    print ('Coordinated Ranking KL Divergence: ' + str(coord_divergence))
-    print ('')
-
-    comp_divergence = kl(ideal_output['R'], comp_output['AR'])
-    print ('Competitve Ranking KL Divergence: ' + str(comp_divergence))
-    print ('')
-
-    ideal_sparsity = numpy.count_nonzero(ideal_output['M'])/ideal_output['M'].size
-    print ('Idealized Mask Sparsity: ' + str(ideal_sparsity))
+    divergence = kl(coord_output['R'], comp_output['R'])
+    print ('Ranking KL Divergence: ' + str(divergence))
     print ('')
 
     coord_sparsity = numpy.count_nonzero(coord_output['M'])/coord_output['M'].size
@@ -351,30 +268,12 @@ def trial(hparams):
     print ('Competitive Mask Sparsity: ' + str(comp_sparsity))
     print ('')
 
-    coord_ideal_poa = sum(coord_output['P']) / sum(ideal_output['P'])
-    print ('Coord-Ideal Price of Anarchy: ' + str(coord_ideal_poa))
-
-    ideal_poa = sum(comp_output['P']) / sum(ideal_output['P'])
-    print ('Comp-Ideal Price of Anarchy: ' + str(ideal_poa))
-
-    coord_poa = sum(comp_output['P']) / sum(coord_output['P'])
-    print ('Comp-Coord Price of Anarchy: ' + str(coord_poa))
-
-    return ideal_poa
-
-    # print ('Comp-ideal Matrix distance:')
-    # print (spatial.distance.cdist(comp_output['W'], ideal_output['W'], 'minkowski', p=2.))
-    # print ('Comp-coord Matrix distance:')
-    # print (spatial.distance.cdist(comp_output['W'], coord_output['W'], 'minkowski', p=2.))
-    # print ('Coord-ideal Matrix distance:')
-    # print (spatial.distance.cdist(coord_output['W'], ideal_output['W'], 'minkowski', p=2.))
-
+    poa = sum(comp_output['P']) / sum(coord_output['P'])
+    print ('Price of Anarchy: ' + str(poa))
 
 def main(hparams):
-    poa = []
     for _ in range(hparams.trials):
-        poa.append(trial(hparams))
-    print (poa)
+        trial(hparams)
 
 
 if __name__ == "__main__":
@@ -389,7 +288,7 @@ if __name__ == "__main__":
         type=int,
         help="Number of trials to run.")
     parser.add_argument(
-        '--max_steps',
+        '--n_steps',
         default=1000,
         type=int,
         help="Number of convergence steps to run.")
